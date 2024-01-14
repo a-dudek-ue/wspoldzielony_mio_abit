925a87572e46b97cd244538c952ad6764f578414
andrzej.dudek <aandrzej.dudek@ue.wroc.pl>
2024-01-14 13:28:25 +0100

    Apple

--- wykres.py
+++ wykres.py
@@ -2,12 +2,12 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 
 # Fetch historical stock data for Amazon
-amazon_stock = yf.download('AMZN', start='2020-01-01', end='2023-01-01')
+amazon_stock = yf.download('AAPL', start='2020-01-01', end='2023-01-01')
 
 # Plotting Amazon stocks in blue
 plt.figure(figsize=(10, 6))
 plt.plot(amazon_stock['Close'], color='blue', label='Amazon Stock (AMZN)')
-plt.title('Amazon Stock Prices')
+plt.title('Apple Stock Prices')
 plt.xlabel('Date')
 plt.ylabel('Stock Price (USD)')
 plt.legend()
